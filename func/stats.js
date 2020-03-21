45b2e19192b250cfb6f4e557bc8f76544c5a6286
vinicius7199 <60449521+vinicius7199@users.noreply.github.com>
2020-03-21 15:31:23 -0300

    Add files via upload

--- func/stats.js
+++ func/stats.js
@@ -8,13 +8,25 @@ bot.on("message", async message => {
 })
 
 bot.on('ready', () => {
-setInterval(function () { bot.user.setActivity(`Newark Roleplay - Precisa de ajuda? !ajuda`); }, 10000);
+  setInterval(function () { bot.user.setActivity(`Newark Roleplay - Precisa de ajuda? !ajuda`); }, 10000);
 })
 
 bot.on("guildMemberAdd", member => {
-  const channel = member.guild.channels.get("690582506101145670"); channel.send(member.user + ` Chegou no **${member.guild.name}**. Bem vindo(a)!`);
+  let avatarr = member.user.displayAvatarURL;
+  let embed = new Discord.RichEmbed()
+    .setThumbnail(avatarr)
+    .setDescription(member.user + ` Chegou no **${member.guild.name}**. Seja bem vindo!`)
+    .setColor("RANDOM")
+    .setTimestamp()
+  const channel = member.guild.channels.get("690582506101145670"); channel.send(embed)
 });
 bot.on("guildMemberRemove", member => {
-          const channel = member.guild.channels.get("690582506101145670"); channel.send(member.user + ` Saiu do **${member.guild.name}**. Espero que volte :C`);
+  let avatarr = member.user.displayAvatarURL;
+  let embed = new Discord.RichEmbed()
+    .setThumbnail(avatarr)
+    .setDescription(member.user + ` Saiu do **${member.guild.name}**. Espero que volte :C`)
+    .setColor("RANDOM")
+    .setTimestamp()
+  const channel = member.guild.channels.get("690582506101145670"); channel.send(embed)
 });
 bot.login(config.token)
--- func/verifica.js
+++ func/verifica.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("../config.json");
 
-bot.on('raw', async dados => {  
+bot.on('raw', async dados => {
     if (dados.t !== "MESSAGE_REACTION_ADD" && dados.t !== "MESSAGE_REACTION_REMOVE") return
     if (dados.d.message_id != "690659056658153482") return
 
-    let servidor = bot.guilds.get("657352699758772242") 
+    let servidor = bot.guilds.get("657352699758772242")
 
     let membro = servidor.members.get(dados.d.user_id)
 
