4cfc8f21afe9e16bfd3e791230772bdabbb8ec90
vinicius7199 <60449521+vinicius7199@users.noreply.github.com>
2020-03-21 22:15:39 -0300

    Update nome.js

--- comandos/nome.js
+++ comandos/nome.js
@@ -6,21 +6,24 @@ exports.run = async (bot, message, args, comando) => {
     if (!message.content.startsWith(config.prefix)) return;
     let msgs = args.join(' ')
 
-    if (message.content.toLowerCase() === `!nick`, msgs) {
+    if (message.content.toLowerCase() === `!nick`, msgs) {  
 
-        message.delete().catch(O_o => { });
-        let avatarr = message.author.iconURL;
-        let pEmbed = new Discord.RichEmbed()
-            .setThumbnail(avatarr)
-            .setTitle("Troca de nome")
-            .addField(`${message.author.tag}`, ` Trocou seu nome para ` + msgs)
-            .setColor("ff6600")
-            .setFooter('Newark Roleplay')
-            .setTimestamp()
-        message.channel.send(pEmbed)
+    message.delete().catch(O_o => { });
+    let avatarr = message.author.iconURL;
+    let pEmbed = new Discord.RichEmbed()
+        .setThumbnail(avatarr)
+        .setTitle("Troca de nome")
+        .addField(`${message.author.tag}`, ` Trocou seu nome para ` + msgs)
+        .setColor("ff6600")
+        .setFooter('Vyth')
+        .setTimestamp()
+    message.channel.send(pEmbed)
+    message.member.setNickname(msgs)
+        .then(m => {
+            m.react("✅");
+        });
 
-
-    }
+}
 }
 exports.help = {
     name: "nick",
