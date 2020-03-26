80d7cb10f0c58ddc43d234f79bf75c1807e1a50f
vinicius7199 <60449521+vinicius7199@users.noreply.github.com>
2020-03-26 18:04:49 -0300

    Add files via upload

---
+++ pmin.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require("../config.json");
+const moment = require ("moment-timezone")
+moment.locale('pt-br')
+
+exports.run = async (bot, message, args) => {
+if (!message.content.startsWith(config.prefix)) return;
+message.delete().catch(O_o => { });
+
+if (!message.member.roles.find(role => role.name === 'LSPD')) return message.reply("Você não tem permissão!")
+
+    let canal = bot.channels.get("657353530163593217")
+    let nome = message.author
+    let avatarr = message.author.displayAvatarURL;
+
+        let pEmbed = new Discord.RichEmbed()
+        .setTitle("Inicio Expediente")
+        .setThumbnail(avatarr)
+        .setDescription(`Oficial - ${nome}\n\n Entrada: - ${moment().format('L')} às ${moment().tz('America/Sao_Paulo').format('LT')}`)
+        .setColor("GREEN")
+        .setFooter('Newark Roleplay')
+        .setTimestamp()
+      canal.send(pEmbed) 
+}
+exports.help = {
+    name: `expin`,
+    aliases: []
+}
